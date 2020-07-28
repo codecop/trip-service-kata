11a09abdebdd17e8037853209526d3883fc137ec
Peter Kofler <peter.kofler@code-cop.org>
2020-07-28 23:46:50 +0200

    . Test test_list_trips_when_friend.

--- python-objects/TripServiceTest.py
+++ python-objects/TripServiceTest.py
@@ -1,5 +1,6 @@
 import unittest
 
+from Trip import Trip
 from TripService import TripService
 from User import User
 from UserNotLoggedInException import UserNotLoggedInException
@@ -13,7 +14,7 @@ class TripServiceTest(unittest.TestCase):
         try:
             trip_service._get_trips_pure(for_user,
                                          lambda: FakeUserSession(None),
-                                         lambda: None)
+                                         lambda user: None)
             self.fail("Should raise an exception")
         except UserNotLoggedInException:
             pass  # ok
@@ -25,9 +26,22 @@ class TripServiceTest(unittest.TestCase):
         trip_service = TripService()
         trips = trip_service._get_trips_pure(for_user,  #
                                              lambda: FakeUserSession(logged_in_user),
-                                             lambda: None)
+                                             lambda user: None)
 
         self.assertEquals([], trips)
+
+    def test_list_trips_when_friend(self):
+        logged_in_user = User()
+        for_user = User()
+        for_user.add_friend(logged_in_user)
+        users_trips = [Trip()]
+
+        trip_service = TripService()
+        trips = trip_service._get_trips_pure(for_user,  #
+                                             lambda: FakeUserSession(logged_in_user),
+                                             lambda user: users_trips)
+
+        self.assertEquals(users_trips, trips)
 
 
 class FakeUserSession(object):
